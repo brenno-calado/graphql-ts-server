fe83a2c6b145fb37ca59214787ce485722138f6a
Brenno Nascimento <brenno.nascimento@saudeid.com.br>
2022-02-02 18:02:19 -0300

    chores: initial setup

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = 9001
+
+app.get('/health', (req, res) => res.status(200).send('Server is running!'))
+
+app.listen(PORT);
+
+console.log(`server is listening on port ${PORT}...\n`)
